4ae1abfc1c862f54d242ba08bd6f12ea80188484
Brianonchari <oncharibrian@gmail.com>
2019-01-22 15:20:19 +0300

    rest services

--- app/src/main/java/com/example/brian/stawika/LoginActivity.java
+++ app/src/main/java/com/example/brian/stawika/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.brian.stawika;
 
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,22 +9,28 @@ import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.brian.stawika.Api.Api;
+import com.example.brian.stawika.Api.ApiClient;
+import com.example.brian.stawika.Model.Users;
 import com.example.brian.stawika.Model.UsersResponse;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+
 import retrofit2.converter.gson.GsonConverterFactory;
 
 
-
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements DialogInterface.OnClickListener {
 
     private Button mButton;
     private static Retrofit retrofit = null;
+    private EditText edit;
 
     private final String API_KEY = "/api/register/register-user" ;
     public static final String BASE_URL = "https://api-test.stawika.com";
@@ -37,18 +44,17 @@ public class LoginActivity extends AppCompatActivity {
         Log.d("LoginActivity","");
 
 //        set cursor position
-        final EditText edit = findViewById(R.id.hint);
+        edit = findViewById(R.id.phone);
         edit.requestFocus();
         edit.setSelection(edit.length());
 
-//intsance of retrofit
 
-
-        Api api = retrofit.create(Api.class);
-        Call<UsersResponse> call = api.getAllUsers();
+        Api api = ApiClient.getClient().create(Api.class);
+        Call<UsersResponse> call = api.createuser(API_KEY);
         call.enqueue(new Callback<UsersResponse>() {
             @Override
             public void onResponse(Call<UsersResponse> call, Response<UsersResponse> response) {
+//                List<Users> users = response.body().getUsers();
 
             }
 
@@ -80,5 +86,10 @@ public class LoginActivity extends AppCompatActivity {
     public void resetPin(View view) {
         Intent i = new Intent(LoginActivity.this,ResetPin.class);
         startActivity(i);
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+
     }
 }
--- app/src/main/java/com/example/brian/stawika/SignUp.java
+++ app/src/main/java/com/example/brian/stawika/SignUp.java
@@ -4,21 +4,55 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.brian.stawika.Api.Api;
+import com.example.brian.stawika.Api.ApiClient;
+import com.example.brian.stawika.Model.UsersResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
 public class SignUp extends AppCompatActivity {
+    public static final String BASE_URL = "https://api-test.stawika.com";
+    private final String API_KEY = "/api/register/register-user";
+    private EditText edit;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        final EditText edit = findViewById(R.id.enter_phone);
-        edit.requestFocus();
-        edit.setSelection(edit.length());
 
+        button = findViewById(R.id.btn_sign_up);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUser();
+            }
+        });
 
+    }
 
+    private void createUser() {
+        Retrofit retrofit = ApiClient.getClient();
+        Api api = ApiClient.getClient().create(Api.class);
+        Call<UsersResponse> call = api.createuser(API_KEY);
+
+        call.enqueue(new Callback<UsersResponse>() {
+            @Override
+            public void onResponse(Call<UsersResponse> call, Response<UsersResponse> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<UsersResponse> call, Throwable t) {
+
+            }
+        });
     }
 
     public void signUp(View view) {
